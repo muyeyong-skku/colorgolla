f5519423a4a948cbe2967ba0232178cf692b0e0a
yewon <yewon.choi.325@gmail.com>
2021-04-17 19:32:13 +0900

    버튼을 추가했음

--- app/src/main/java/com/example/muyeyong/MainActivity.java
+++ app/src/main/java/com/example/muyeyong/MainActivity.java
@@ -6,14 +6,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-// 홈 화면을 만드는 중
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    // 색상 컴포넌트 만들기
-
-    // 리스트 만들기
 }
