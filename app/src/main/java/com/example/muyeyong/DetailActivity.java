0223d36c674355cffd00a3e8a25d65776390d0b8
yewon <yewon.choi.325@gmail.com>
2021-04-18 22:51:35 +0900

    드디어 화면 전환이 다 된다~

--- app/src/main/java/com/example/muyeyong/DetailActivity.java
+++ app/src/main/java/com/example/muyeyong/DetailActivity.java
@@ -2,7 +2,14 @@ package com.example.muyeyong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +17,42 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-    }
-}
+        ImageButton btn = (ImageButton) findViewById(R.id.viewmore);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                PopupMenu popup= new PopupMenu(getApplicationContext(), v);//v는 클릭된 뷰를 의미
+
+                getMenuInflater().inflate(R.menu.option_menu, popup.getMenu());
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.m1:
+                                Toast.makeText(getApplication(),"홈 화면입니다",Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                break;
+                            case R.id.m2:
+                                Toast.makeText(getApplication(),"나의 프로필입니다",Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), UserActivity.class));
+                                break;
+                            case R.id.m3:
+                                Toast.makeText(getApplication(),"색상 상세보기 입니다",Toast.LENGTH_SHORT).show();
+                                break;
+                            default:
+                                break;
+                        }
+                        return false;
+                    }
+                });
+
+                popup.show();//Popup Menu 보이기
+            }
+        });
+    }}
+
+
+
+
+
+
--- app/src/main/java/com/example/muyeyong/UserActivity.java
+++ app/src/main/java/com/example/muyeyong/UserActivity.java
@@ -2,7 +2,13 @@ package com.example.muyeyong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -10,5 +16,42 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
-    }
-}
+        ImageButton btn = (ImageButton) findViewById(R.id.viewmore);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                PopupMenu popup= new PopupMenu(getApplicationContext(), v);//v는 클릭된 뷰를 의미
+
+                getMenuInflater().inflate(R.menu.option_menu, popup.getMenu());
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.m1:
+                                Toast.makeText(getApplication(),"홈 화면입니다",Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                break;
+                            case R.id.m2:
+                                Toast.makeText(getApplication(),"나의 프로필입니다",Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.m3:
+                                Toast.makeText(getApplication(),"색상 상세보기 입니다",Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), DetailActivity.class));
+                                break;
+                            default:
+                                break;
+                        }
+                        return false;
+                    }
+                });
+
+                popup.show();//Popup Menu 보이기
+            }
+        });
+    }}
+
+
+
+
+
+
