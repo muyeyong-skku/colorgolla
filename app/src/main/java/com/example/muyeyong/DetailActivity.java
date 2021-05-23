69ff6f464f3f39387e7444fe7e27816f5c87fbc1
dee <deedcde>
2021-05-23 19:29:01 +0900

    버튼 지정

--- app/src/main/java/com/example/muyeyong/DetailActivity.java
+++ app/src/main/java/com/example/muyeyong/DetailActivity.java
@@ -6,25 +6,38 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileOutputStream;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity  {
 
     Button sharebtn;
+    Button savebtn;
+
+    ImageView imageView;
+    File file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        File sdcard = Environment.getExternalStorageDirectory();
+        file = new File(sdcard, "colorimage9.png");
+
+        imageView = findViewById(R.id.imageView9);
+
         ImageButton sharebtn = (ImageButton) findViewById(R.id.sharebtn);
+        ImageButton savebtn = (ImageButton) findViewById(R.id.savebtn);
         sharebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,30 +50,34 @@ public class DetailActivity extends AppCompatActivity {
 
                 Intent Sharing = Intent.createChooser(Sharing_intent, "공유하기");
                 startActivity(Sharing);
-        }
-           // public void isave(View view)
-           // {
-                //isave();
-           // }
+            }
+        });
+        savebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                download();
+            }
+        });
+    }
 
-            //public void isave()
-            //{
-               //String filename = "myfile";
-              //  String string = "Hello world!";
-              //  FileOutputStream outputStream;
+    public void download(){
+        file.
+               String filename = "myfile";
+                String string = "Hello world!";
+                FileOutputStream outputStream;
 
-               // try {
-                //    outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
-               //     outputStream.write(string.getBytes());
-               //     outputStream.close();
-//
-               //     Toast.makeText(this, "this is internal storage save success.", Toast.LENGTH_LONG).show();
-              //  } catch (Exception e) {
-                 //   e.printStackTrace();
+                try {
+                    outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+                    outputStream.write(string.getBytes());
+                    outputStream.close();
 
-                  //  Toast.makeText(this, "this is internal storage save fail.", Toast.LENGTH_LONG).show();
-                //}
-            //}
+                    Toast.makeText(this, "this is internal storage save success.", Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+
+                    Toast.makeText(this, "this is internal storage save fail.", Toast.LENGTH_LONG).show();
+                }
+            }
         });
     }}
 
