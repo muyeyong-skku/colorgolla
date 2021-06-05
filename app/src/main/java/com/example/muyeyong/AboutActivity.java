e4c620ca941cf6c1433af1f7f227a0a1bd9d3b52
sircoon4 <sircoon4@gmail.com>
2021-06-05 14:31:20 +0900

    remove about activity contents

--- app/src/main/java/com/example/muyeyong/AboutActivity.java
+++ app/src/main/java/com/example/muyeyong/AboutActivity.java
@@ -1,38 +1,12 @@
 package com.example.muyeyong;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.util.Log;
-import android.view.View;
-
-import android.database.sqlite.SQLiteDatabase;
-
-import java.util.ArrayList;
 
 public class AboutActivity extends AppCompatActivity {
-    private String DATABASE_NAME = "mydb.db";
-    ArrayList<color> al;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-
-        DBHandler dbhandler = new DBHandler(getApplicationContext());
-
-        Log.i("test","test");
-
-        al = dbhandler.getColors();
-
-        Log.i("test1","test1");
     }
 }
